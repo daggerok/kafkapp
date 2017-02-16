dfaeb5aad125b899886da43a7c5c640e62a52a22
Maksim Kostromin <daggerok@gmail.com>
2017-02-16 05:15:10 +0200

    emulate batch job for statistics collection

--- README.md
+++ README.md
@@ -2,20 +2,10 @@ kafkapp [![build](https://travis-ci.org/daggerok/kafkapp.svg?branch=master)](htt
 =======
 
 ```fish
-docker-compose --project-name=kafkapp up
-```
-
-```fish
-docker-compose --project-name=kafkapp up
-
-rm -rf build/kafka-docker
-git clone -b 0.10.0 https://github.com/wurstmeister/kafka-docker.git build/kafka-docker
-# edit build/kafka-docker/docker-compose-single-broker.yml 
-# comment KAFKA_ADVERTISED_HOST_NAME
-# update 'topics'
-docker-compose --project-name=kafkapp -f build/kafka-docker/docker-compose-single-broker.yml up
-```
-
-```fish
-gradle --parallel bootRun
+docker-compose start
+# or
+docker-compose --project-name=kafkapp up -d
+gradle payment-query:bootRun
+gradle payment-command:bootRun
+docker-compos stop
 ```
--- payment-command/src/main/java/daggerok/config/ConsumerConfig.java
+++ payment-command/src/main/java/daggerok/config/ConsumerConfig.java
@@ -8,6 +8,8 @@ import org.springframework.integration.annotation.ServiceActivator;
 
 import java.math.BigInteger;
 
+import static java.lang.System.currentTimeMillis;
+
 @Configuration
 
 @Slf4j
@@ -15,9 +17,18 @@ import java.math.BigInteger;
 public class ConsumerConfig {
 
     static BigInteger counter = BigInteger.ZERO;
+    static long latest = -1;
 
     @ServiceActivator(inputChannel = Sink.INPUT)
-    public void logMessage(String message) {
-        log.info("received {}: '{}'", counter = counter.add(BigInteger.ONE), message);
+    public void logMessage(String payload) {
+
+        if (latest > 0 && currentTimeMillis() - latest > 100L) {
+
+            log.info("statistics collect finished.");
+            System.exit(0);
+        }
+
+        log.info("{}) payload: '{}', latest: {}",
+                counter = counter.add(BigInteger.ONE), payload, latest = currentTimeMillis());
     }
 }
